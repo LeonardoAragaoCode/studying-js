4fa84904dd4d0384f8ab01d45689a9261aff0f4a
leonardoaragaocode <you@example.com>
2021-08-11 15:57:35 -0300

    estudando

--- Fundamentos/funcao.js
+++ Fundamentos/funcao.js
@@ -1,4 +1,5 @@
 //Função sem retorno
+console.log("Função sem retorno")
 function imprimirSoma(a, b) {
     console.log(a + b)
 }
@@ -9,6 +10,7 @@ imprimirSoma(2,10,4,5,6,7,8,9,) //Todo resto é ignorado. Somente é considerado
 imprimirSoma() // "undefined" + "undefined" = NaN(Not a Number)
 
 // Função com retorno
+console.log("Função com retorno")
 function soma(a, b = 1) {
     return a + b
 }
@@ -18,6 +20,26 @@ console.log(soma(2)) // 2 + b(no caso, se não tiver nada, ele é preenchido com
 console.log(soma()) // a(undefined) + b(1) = NaN
 
 // Armazenando uma função em uma variável
-const imprimirSubtracao = function (a,b) {
-    
+console.log("Função em uma variável")
+const imprimirSubtracao = function (a, b) {
+    console.log(a - b)
 }
+
+imprimirSubtracao(5, 4)
+
+//Armazenando uma função arrow em uma variável
+console.log("Função arrow em uma variável")
+const subtração = (a, b) => {
+    return a - b
+}
+console.log(subtração(10, 8))
+
+//retorno implícito
+console.log("retorno implícito")
+const multiplicacao = (a, b) => a * b 
+console.log(multiplicacao(2, 2))
+
+const imprimir2 = a => console.log(a)
+imprimir2("Legal!!!")
+
+
