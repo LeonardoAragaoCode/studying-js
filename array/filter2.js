e562833e46ed5dfaf49dbb83e18e0fa6d95704d7
leonardoaragaocode <85124953+leonardoaragaocode@users.noreply.github.com>
2021-09-20 17:02:15 -0300

    anotacoes

---
+++ array/filter2.js
@@ -0,0 +1,10 @@
+//Simulação de um ".filter":
+Array.prototype.filter2 = function(callback) {
+    const newArray = []
+    for (let i = 0; i < this.length; i++) {
+        if(callback(this[i], i, this)) {
+            newArray.push(this[i])
+        }
+    }
+    return newArray
+}
