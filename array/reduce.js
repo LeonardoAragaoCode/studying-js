5794cfc688c987d770fbc36db193d2164d568b51
leonardoaragaocode <85124953+leonardoaragaocode@users.noreply.github.com>
2021-09-20 23:09:50 -0300

    anotacoes

---
+++ array/reduce.js
@@ -0,0 +1,23 @@
+const alunos = [
+    {nome: "João", nota: 6.5, bolsista: false},
+    {nome: "Guilherme", nota: 8.5, bolsista: true},
+    {nome: "Beatriz", nota: 10, bolsista: false},
+    {nome: "Ana", nota: 7.0, bolsista: true}
+]
+
+console.log(alunos.map(a => a.nota)) //Cria um array somente com as notas
+
+//".reduce" tem uma função de acumular arrays e fazer uma operação que voce preferir
+
+const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual){
+    console.log(acumulador, atual) //Só para ver como funciona o processo
+    return acumulador + atual
+})
+
+/* Posso também colocar um número inicial ao invés do 6.5
+* Observe a posição na qual ele deve estar:
+const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual){
+    console.log(acumulador, atual) 
+    return acumulador + atual
+}, 50)
+*/
